2ccd83660d03850f6fbe2c78f9dfc1421113c880
Frenco <hey@frenco.dev>
2021-02-25 11:23:31 +0630

    feat: add bucket urls to allowed domains

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
+const buckets = ['vouchedbyme80bc3466fb0848498adf85f3950f9b87234407-dev']
+
 module.exports = {
   images: {
-    domains: ['localhost'],
+    domains: [
+      'localhost',
+      ...buckets.map((bucket) => `${bucket}.s3.us-east-1.amazonaws.com`),
+    ],
   },
 }
