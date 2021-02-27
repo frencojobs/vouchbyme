b7717c35299f2a84500e046729d68ff61eaa78cd
Frenco <hey@frenco.dev>
2021-02-27 23:11:45 +0630

    feat: remove footer from dashboard

--- components/layouts/dashboard/Footer.tsx
+++ components/layouts/dashboard/Footer.tsx
@@ -1,11 +1,11 @@
-import { Text } from '@geist-ui/react'
+// import { Text } from '@geist-ui/react'
 
 export const Footer: React.FC = () => {
-  const year = new Date().getFullYear()
+  // const year = new Date().getFullYear()
 
   return (
     <div className="max-w-4xl px-5 pt-5 mx-auto">
-      <Text>{year} &copy; VouchedByMe</Text>
+      {/* <Text>{year} &copy; VouchedByMe</Text> */}
     </div>
   )
 }
