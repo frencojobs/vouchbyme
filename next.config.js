4247d8c2b4f7d31cf86e4eeea440b3dc3fcdd689
Frenco <hey@frenco.dev>
2021-02-28 15:15:19 +0630

    feat: add cloudfront url as host

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
+const host = 'd2mylahvc7nc1a.cloudfront.net'
 const buckets = ['vouchedbyme80bc3466fb0848498adf85f3950f9b87234407-dev']
 
 module.exports = {
   images: {
     domains: [
       'localhost',
+      host,
       ...buckets.map((bucket) => `${bucket}.s3.us-east-1.amazonaws.com`),
     ],
   },
